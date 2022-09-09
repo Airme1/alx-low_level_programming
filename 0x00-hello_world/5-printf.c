fbd4d8ed3c800510e3f6d25a9d92b30ee9e31662
Airme1 <emmanuelochoja@yahoo.com>
2022-09-09 05:36:43 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ *main: indicates where execution starts
+ *Return: indicates where it ends
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
