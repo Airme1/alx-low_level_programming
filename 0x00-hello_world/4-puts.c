224c541eee08d34b7cf01ae138787d44514b2cb8
Airme1 <emmanuelochoja@yahoo.com>
2022-09-09 05:20:27 +0100

    4-puts.c commenting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*main Program to print on screen *
- * return indicated program has ended*/
+/*
+ * main Program to print on screen
+ * return indicated program has ended
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
