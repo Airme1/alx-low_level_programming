4a5806e4ab66c4919412a022c40d74ef672bc00b
Airme1 <emmanuelochoja@yahoo.com>
2022-09-09 05:03:05 +0100

    4-puts.c betty follow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-	#include <stdio.h>
-	/*Program to print on screen*/
-	
-	int main(){
-		puts("Programming is like building a multilingual puzzle\n");
-		return 0;
-	}
+#include <stdio.h>
+/*Program to print on screen*/
+
+int main()
+{
+puts("Programming is like building a multilingual puzzle\n");
+return 0;
+}
