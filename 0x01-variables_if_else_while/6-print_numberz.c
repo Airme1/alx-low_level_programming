68213af20fc297e8d746470c9b106bb3452ef356
Airme1 <emmanuelochoja@yahoo.com>
2022-09-10 05:43:37 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 to terminate function
+ */
+int main(void)
+{
+        int n = 0;
+
+        while (n < 10)
+        {
+                putchar(n);
+                n++;
+        }
+        put('\n');
+        return (0);
+}
+
