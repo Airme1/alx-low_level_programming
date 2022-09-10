e7f6ff0a2f39476f5c806815d3fd28dccd76f731
Airme1 <emmanuelochoja@yahoo.com>
2022-09-10 05:38:01 +0100

    5-print_numbers.c correct

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - main function
+ * Return: 0 to terminate function
+ */
 int main(void)
 {
 	int n = 0;
+
 	while (n < 10)
 	{
-		printf("%d",n);
+		printf("%d", n);
 		n++;
 	}
+	printf("\n");
 	return (0);
 }
