93edce683906d9ece482ec4b66b63efd43bcbbed
Airme1 <emmanuelochoja@yahoo.com>
2022-09-14 04:38:36 +0100

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *times_table -> time table
+ */
+void times_table(void)
+{
+	int num1, num2, product;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			product = num2 * num2;
+			if (num2 == 0)
+				_putchar(product + '0');
+			if (num2 != 0 && product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			} 
+			else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
