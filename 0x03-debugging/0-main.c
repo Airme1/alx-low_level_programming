96622b3b7698627a9180eb81c3cf6b3ad1c5d27d
Airme1 <emmanuelochoja@yahoo.com>
2022-09-16 03:35:59 +0100

    dealing with betty task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer 0 is positive or negative
 * Return: 0
 */
 
