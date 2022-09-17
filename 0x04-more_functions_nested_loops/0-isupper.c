1e2e1eff95f6b4877beb2ce5d91aefd94910cddc
Airme1 <emmanuelochoja@yahoo.com>
2022-09-17 05:14:47 +0100

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isupper- validates uppercase characters
+ * @c: character to be validated of int type
  * Return: 1 if the character is upper, and 0 if not
  */
 
-int _isupper(int c)
+int _isupper (int c)
 {
-	if(c >='A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
