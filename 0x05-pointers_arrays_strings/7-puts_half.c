4620b34cd3b8b609c802ac5dd7381dc3bc4e4863
Airme1 <emmanuelochoja@yahoo.com>
2022-09-20 06:13:39 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
- * puts2- print every other character in a string
- * @str: string pointer
+ * puts_half - print half of a string
+ * @str: pointer to string
  */
 
-void puts2(char *str)
+void puts_half(char *str)
 {
+	int total = 0;
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0 || i % 2 == 0)
-			_putchar(str[i]);
-	}
+	for (i = 0; *(str + i) != '\0'; i++)
+		total++;
+
+	if (total % 2 == 0)
+		total = total / 2;
+	else
+		total = ((total - 1) / 2) + 1;
+
+	for (i = total; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
 
 	_putchar('\n');
 }
